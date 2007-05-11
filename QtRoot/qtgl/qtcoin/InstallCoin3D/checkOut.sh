5487e5c25bae954dfd1a74279ffe02947e1c8890
fine <>
2007-05-11 14:26:24 +0000

    Add Win32 test

--- QtRoot/qtgl/qtcoin/InstallCoin3D/checkOut.sh
+++ QtRoot/qtgl/qtcoin/InstallCoin3D/checkOut.sh
@@ -17,22 +17,25 @@ echo "See: http://www.coin3d.org/doc/cvs_access"
 
 export COINCVSROOT=:pserver:cvs@cvs.coin3d.org:/export/cvsroot
 export COINSVNROOT=https://svn.coin3d.org/repos
+testPlatform=`uname | grep -c CYGWIN`
+if test "x$testPlatform" != "x0" ; then
 export PATH="`cygpath "E:/Program Files/svn-win32-1.4.2/bin"`":$PATH
 echo $PATH
+fi
 # "Coin" - Latest development version
 # cvs -z3 -d $COINCVSROOT checkout Coin
 
 # "Coin-2" - Last stable production release
 svn co ${COINSVNROOT}/Coin-2/trunk       Coin-2
 
-svn co ${COINSVNROOT}/SmallChange/trunk  SmallChange
+echo svn co ${COINSVNROOT}/SmallChange/trunk  SmallChange
 svn co ${COINSVNROOT}/simage/trunk       simage
 svn co ${COINSVNROOT}/simacros/trunk     simacros
 svn co ${COINSVNROOT}/SoQt/trunk         SoQt
 
 testPlatform=`uname | grep -c CYGWIN`
 if test "x$testPlatform" = "x0" ; then
-#  svn -z3 $COINCVNROOT checkout SoXt
+echo  svn -z3 $COINSVNROOT checkout SoXt
 fi
 
-svn co ${COINCVNROOT}/SoGuiExamples/trunk SoGuiExamples
+svn co ${COINSVNROOT}/SoGuiExamples/trunk SoGuiExamples
