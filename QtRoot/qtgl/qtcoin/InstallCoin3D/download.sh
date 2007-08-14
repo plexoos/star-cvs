1b978aac022afc6a7132c3a836f3655f6c8073db
fine <>
2007-08-14 22:43:43 +0000

    Download Coint via svn

--- QtRoot/qtgl/qtcoin/InstallCoin3D/download.sh
+++ QtRoot/qtgl/qtcoin/InstallCoin3D/download.sh
@@ -1,2 +1,7 @@
-wget http://ftp.coin3d.org/coin/src/all/Coin-2.4.6.tar.gz
-wget http://ftp.coin3d.org/coin/src/all/SoQt-1.4.1.tar.gz
+svn co https://svn.coin3d.org/repos/Coin-2/trunk Coin-2
+svn co https://svn.coin3d.org/repos/SoQt/trunk SoQt
+svn co https://svn.coin3d.org/repos/SoGuiExamples/trunk SoGuiExamples
+svn co https://svn.coin3d.org/repos/simage/trunk simage
+svn co https://svn.coin3d.org/repos/SmallChange/trunk SmallChange
+ #wget http://ftp.coin3d.org/coin/src/all/Coin-2.4.6.tar.gz
+#wget http://ftp.coin3d.org/coin/src/all/SoQt-1.4.1.tar.gz
