77fd4be23430b4fcbedc70bc02252ce72991e87d
fine <>
2007-08-27 18:57:12 +0000

    Fix the greek latter issue. Thank Jim Thomas

--- QtRoot/qt/src/TGQt.cxx
+++ QtRoot/qt/src/TGQt.cxx
@@ -1,7 +1,7 @@
-// @(#)root/qt:$Name:  $:$Id: TGQt.cxx,v 1.20 2007/08/25 02:31:55 fine Exp $
+// @(#)root/qt:$Name:  $:$Id: TGQt.cxx,v 1.21 2007/08/27 18:57:12 fine Exp $
 // Author: Valeri Fine   21/01/2002
 /****************************************************************************
-** $Id: TGQt.cxx,v 1.20 2007/08/25 02:31:55 fine Exp $
+** $Id: TGQt.cxx,v 1.21 2007/08/27 18:57:12 fine Exp $
 **
 ** Copyright (C) 2002 by Valeri Fine. Brookhaven National Laboratory.
 **                                    All rights reserved.
@@ -730,7 +730,7 @@ Bool_t TGQt::Init(void* /*display*/)
 {
    //*-*-*-*-*-*-*-*-*-*-*-*-*-*Qt GUI initialization-*-*-*-*-*-*-*-*-*-*-*-*-*-*
    //*-*                        ========================                      *-*
-   fprintf(stderr,"** $Id: TGQt.cxx,v 1.20 2007/08/25 02:31:55 fine Exp $ this=%p\n",this);
+   fprintf(stderr,"** $Id: TGQt.cxx,v 1.21 2007/08/27 18:57:12 fine Exp $ this=%p\n",this);
 #if QT_VERSION >= 0x40000
 #ifndef R__QTWIN32
    extern void qt_x11_set_global_double_buffer(bool);
@@ -830,7 +830,7 @@ Bool_t TGQt::Init(void* /*display*/)
     Bool_t symbolFontFound = kFALSE;
     Bool_t isXdfSupport = 
 #if QT_VERSION < 0x40000          
-          kTRUE;
+          kFALSE;
 #else
           !gSystem->Getenv("QT_X11_NO_FONTCONFIG");
 #endif            
