6c6e12bf082a74c308a4f2678c671ad3e1362772
cvs2git <>
2008-07-20 06:17:25 +0000

    This commit was manufactured by cvs2svn to create tag 'P080720_trg_2005'.

--- QtRoot/MyRules.mk
+++
@@ -1,7 +0,0 @@
-ifeq ($(PLATFORM),win32) 
-# move libThread  from PROOFLIBS to libCore   $(LPATH)/libThread.lib
-  PROOFLIBS    := $(LPATH)/libProof.lib $(LPATH)/libTreePlayer.lib 
-  COREL  +=  $(THREADL)
-  COREO  +=  $(THREADO)
-  COREDO +=  $(THREADDO)
-endif
