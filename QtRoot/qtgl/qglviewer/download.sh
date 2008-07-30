134e73d1a43540440196d1e3f0e00b435001846b
cvs2git <>
2008-07-30 23:53:20 +0000

    This commit was manufactured by cvs2svn to create branch 'SL10k_patch'.

--- QtRoot/qtgl/qglviewer/download.sh
+++
@@ -1,3 +0,0 @@
-# wget http://artis.imag.fr/Members/Gilles.Debunne/QGLViewer/src/libQGLViewer-2.0.0-5.tar.gz
-#wget http://artis.imag.fr/Members/Gilles.Debunne/QGLViewer/src/libQGLViewer-2.1.1-8.tar.gz
-wget http://artis.imag.fr/Members/Gilles.Debunne/QGLViewer/src/libQGLViewer-2.2.1-1.tar.gz
