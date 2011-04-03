349b3b33c0c2d4a8f15c7f97f387fac17fc4b82a
cvs2git <>
2011-04-03 15:52:59 +0000

    This commit was manufactured by cvs2svn to create branch 'testcvsupdate'.

--- QtRoot/plugins/TVirtualPadEditor/P111_TQtGedEditor.C
+++
@@ -1,8 +0,0 @@
-void P111_TQtGedEditor()
-{
-   TString qtgui = gEnv->GetValue("Gui.Factory","native");
-   if (qtgui == "qtgui") {
-      gPluginMgr->AddHandler("TVirtualPadEditor", "QtGed", "TQtGedEditor",
-         "QtGed", "TQtGedEditor(TCanvas*)");
-   }
-}
