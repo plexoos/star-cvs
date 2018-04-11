6120e3e4b9129e9cbb92fb3796ecbdca07d63fb1
cvs2git <>
2018-04-11 02:39:50 +0000

    This commit was manufactured by cvs2svn to create branch 'SL18c_embpatch'.

--- QtRoot/qtExamples/HelloCint/HelloCint.C
+++
@@ -1,9 +0,0 @@
-{
-   // define debug /release extra DLL path for Win32 platform.
-  TString p = "debug:release";
-     p +=  gSystem->GetDynamicPath();
-     gSystem->SetDynamicPath(p);
-  gSystem->Load("libHelloCint");
-  TMyQButton  *button= new  TMyQButton("Hello ROOT!!!");
-  button->Show();
-}
